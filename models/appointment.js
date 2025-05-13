364daaa133b2a67fbc991e9e06037d3f4145546b
Mehak <mehakgarg598@gmail.com>
2025-05-13 08:10:26 +0530

    did

--- models/appointment.js
+++ models/appointment.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+main().catch(err => console.log(err));
 
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Medlink');
+}
 const appointmentSchema = new mongoose.Schema({
   username: { type: String, required: true },
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'Doctor', required: true },
